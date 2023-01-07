72e97cb4f1de569b2dde1dbd4d23ccda37d16e98
kris-slinger <krisdjangodeveloper@gmail.com>
2023-01-07 17:48:37 +0300

    add: tag and tagdetail views

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,50 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Blog
+from .models import Blog,Tag
 from rest_framework.decorators import api_view
-from .serializers import BlogSerializer
+from .serializers import BlogSerializer,TagSerializer
+from django.http import Http404
 class BlogView(APIView):
     def get(self,request):
         query=Blog.objects.all()
-        serialized=BlogSerializer(query,many=True)
-        print(serialized.data)
-        return Response(serialized.data,status=status.HTTP_200_OK)
-# Create your views here.
+        serialize=BlogSerializer(query,many=True)
+        return Response(serialize.data,status=status.HTTP_200_OK)
+    def post(self,request):
+        serializer=BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(writter=request.user)
+            return Response(serializer.validated_data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+class TagView(APIView):
+    def get(self,request):
+        querySet=Tag.objects.all()
+        serializer=TagSerializer(querySet,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    def post(self,request):
+        serializer=TagSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.validated_data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+class TagDetailView(APIView):
+    def get_object(self,pk):
+        try:
+            return Tag.objects.get(id=pk)
+        except Tag.DoesNotExist:
+            raise Http404
+    def get(self,request,pk):
+        query=self.get_object(pk)
+        serializer=TagSerializer(query)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    def put(self,request,pk):
+        query=self.get_object(pk)
+        serializer=TagSerializer(query,data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def delete(self,request,pk):
+        query=self.get_object(pk)
+        query.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
