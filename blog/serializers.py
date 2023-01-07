d9b8f04ace9c35b9c8b545806d1495a8a969bad5
kris-slinger <krisdjangodeveloper@gmail.com>
2023-01-07 17:48:18 +0300

    update: serializer

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 from .models import Blog,Tag
+from users.serializers import CustomUserSerializer
 class TagSerializer(serializers.ModelSerializer):
     class Meta: 
         fields='__all__'
         model=Tag
+
 class BlogSerializer(serializers.ModelSerializer):
     tag=TagSerializer(many=True,read_only=True)
+    writter=CustomUserSerializer(read_only=True)
     class Meta:
         model=Blog
         fields=['id','tag','blogTitle','blogContent','writter']
