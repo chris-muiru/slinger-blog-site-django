7e5d16b134c462cdc8085184d18213a225d6a702
kris-slinger <krisdjangodeveloper@gmail.com>
2023-01-07 17:51:31 +0300

    add: comment and like serializers

---
+++ socials/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Comment,Like
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields='__all__'
+        model=Comment
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields='__all__'
+        model=Like
