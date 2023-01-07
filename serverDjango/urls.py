1d659f2ceded2d5f579e1610c720376eacbf5b36
kris-slinger <krisdjangodeveloper@gmail.com>
2023-01-07 17:50:04 +0300

    update: add auth,blog,user and socials paths

--- serverDjango/urls.py
+++ serverDjango/urls.py
@@ -17,10 +17,14 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include("auth.urls")),
     path('blog/',include("blog.urls")),
-    path('users/',include("users.urls"))
+    path('user/',include("users.urls")),
+    path('socials/',include("socials.urls"))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
