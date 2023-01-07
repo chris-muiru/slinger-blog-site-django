cfdba9fb2f746fdd52babfa1f3df8292e4bb23ac
kris-slinger <krisdjangodeveloper@gmail.com>
2023-01-07 17:55:31 +0300

    add: signup and customuser related views

--- users/views.py
+++ users/views.py
@@ -5,9 +5,21 @@ from rest_framework import status
 from rest_framework.response import Response
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+class SignUpCustomUserView(APIView):
+    def post(self,request):
+        serializer=CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({},status=status.HTTP_201_CREATED) 
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
 class CustomUserView(APIView):
-    def get(self,request):
-        queryset=CustomUser.objects.all()
-        serializer=CustomUserSerializer(queryset,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
-# Create your views here.
+    pass
+ 
+class CustomUserDetailView(APIView):
+    def get(self,request,pk):
+        pass
+    def put(self,request,pk):
+        pass
+    def delete(self,request,pk):
+        pass
