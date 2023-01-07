33728d98079f9dff05b5f9d7c04dc4bb3311ed20
kris-slinger <krisdjangodeveloper@gmail.com>
2023-01-07 17:54:55 +0300

    update: override create function to create new users

--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    def create(self,validated_data):
+        return CustomUser.objects.create_user(**validated_data)
     class Meta:
         model=CustomUser        
         fields='__all__'
