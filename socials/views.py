59d6974f4026c19057eb2c1e65c28a4e0cfe6013
kris-slinger <krisdjangodeveloper@gmail.com>
2023-01-07 17:52:33 +0300

    add: comment and like views

--- socials/views.py
+++ socials/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from .models import Comment,Like
+from .serializers import CommentSerializer,LikeSerializer 
+from rest_framework import status
+from rest_framework.response import Response
 # Create your views here.
+
+
+class CommentView(APIView):
+    def get(self,request):
+        query=Comment.objects.all()
+        serializer=CommentSerializer(query)
+        return Response(serializer.data,status=status.HTTP_200_OK) 
+    def post(self,request):
+        serializer=CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.validated_data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST) 
+
+class LikeView(APIView):
+    def get(self,request):
+        query=Like.objects.all()
+        serializer=LikeSerializer(query)
+        return Response(serializer.data,status=status.HTTP_200_OK) 
+    def post(self,request):
+        serializer=LikeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.validated_data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST) 
