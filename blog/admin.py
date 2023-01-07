e557849be27faa0ba6d1e480f8fabccffc037e39
kris-slinger <krisdjangodeveloper@gmail.com>
2023-01-07 17:44:39 +0300

    update: register models

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Tag,Blog,BookMarkedBlog
+from .models import Tag,Blog,BookmarkedBlog,BlogPic
 admin.site.register(Tag)
 admin.site.register(Blog)
-admin.site.register(BookMarkedBlog)
-
+admin.site.register(BookmarkedBlog)
+admin.site.register(BlogPic)
 # Register your models here.
