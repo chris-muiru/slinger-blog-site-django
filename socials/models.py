3c821bcdaa25d154a9493dca7a77bd557886b452
kris-slinger <krisdjangodeveloper@gmail.com>
2023-01-11 16:54:02 +0300

    update: add and update fields and dunder function

--- socials/models.py
+++ socials/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from blog.models import Blog
 from users.models import CustomUser
+from django.utils import timezone
 class Comment(models.Model):
     blog=models.ForeignKey(Blog,on_delete=models.CASCADE)
     comment=models.CharField(max_length=100)
     sender=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    createdAt=models.DateTimeField(auto_now_add=timezone.now)
     def __str__(self):
-        return self.blog.blogName
+        return self.blog.blogTitle
 # Create your models here.
-
+# 
 class Like(models.Model):
     blog=models.ForeignKey(Blog,on_delete=models.CASCADE)
     sender=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
-    blog_is_liked=models.BooleanField(default=True)
+    is_liked=models.BooleanField(default=True)
     def __str__(self):
-        return self.blog.blogName
+        return self.blog.blogTitle
