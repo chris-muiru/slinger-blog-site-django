cf12b638ec53a39b34fdec5fae3d2ba140bbb239
kris-slinger <krisdjangodeveloper@gmail.com>
2023-01-11 16:56:08 +0300

    update: urlpatterns

--- socials/urls.py
+++ socials/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import CommentView
+from .views import CommentView,LikeView
 urlpatterns=[
-    path('comment/',CommentView.as_view(),name="comments")
-    # path('like/')
+    path('comment/',CommentView.as_view(),name="comments"),
+    path('like/<int:pk>/',LikeView.as_view())
 ]
