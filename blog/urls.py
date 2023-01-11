a2f10dff6102aefea0f22a4f875a0dbb354cb420
kris-slinger <krisdjangodeveloper@gmail.com>
2023-01-11 16:51:21 +0300

    update: urlpattern

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from .views import BlogView,TagView
+from .views import BlogView,TagView,BookmarkedBlogView
 urlpatterns = [
     path('',BlogView.as_view()),
+    path('book/',BookmarkedBlogView.as_view()),
     path('tag/',TagView.as_view())
 ]
